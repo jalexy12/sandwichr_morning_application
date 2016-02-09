20db41fb97067edfab235274e9996f8a8506c7c5
Josh <jalexy12@gmail.com>
2016-02-08 22:45:51 -0500

    Created after_add callback for sandwich total_calories attribute

--- app/models/sandwich.rb
+++ app/models/sandwich.rb
@@ -1,4 +1,10 @@
 class Sandwich < ActiveRecord::Base
 	has_many :sandwich_ingredients
-	has_many :ingredients, through: :sandwich_ingredients
+	has_many :ingredients, through: :sandwich_ingredients, 
+					 after_add: :add_calories
+	private
+
+	  def add_calories(ingredient)
+	  	self.total_calories += ingredient.calories
+	  end
 end
