de46e8d8892ac040a4fcafa7516a6b4624030441
Josh <jalexy12@gmail.com>
2016-02-09 10:23:27 -0500

    Added some seeds for sandwiches and ingredients

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Ingredient.create(name: "Bacon", calories: 120)
+Ingredient.create(name: "Tomato", calories: 25)
+Ingredient.create(name: "Lettuce", calories: 3)
+Ingredient.create(name: "Ham", calories: 140)
+Ingredient.create(name: "Turkey", calories: 130)
+Ingredient.create(name: "Mayo", calories: 200)
+Ingredient.create(name: "Arugala", calories: 50)
+
+Sandwich.create(name: "BLT", bread_type: "Ciabatta")
+Sandwich.create(name: "Reuban", bread_type: "Rye")
+Sandwich.create(name: "Grilled Cheese", bread_type: "White")
