cd823465abd3f028da0190d24e39d44e4192705f
Josh <jalexy12@gmail.com>
2016-02-08 22:39:01 -0500

    Added total_calorie attribute to the sandwich, along with the default value

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160208172423) do
+ActiveRecord::Schema.define(version: 20160209033822) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,8 +36,9 @@ ActiveRecord::Schema.define(version: 20160208172423) do
   create_table "sandwiches", force: :cascade do |t|
     t.string   "name"
     t.string   "bread_type"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+    t.integer  "total_calories", default: 0
   end
 
   add_foreign_key "sandwich_ingredients", "ingredients"
